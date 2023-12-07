7dd0364b5f49966723b383b826839e50be256142
Kim Bolender <kimbolender99@gmail.com>
2023-12-07 12:44:40 +0100

    Trying Drag & Drop

--- Assets/Scripts/InventoryItem.cs
+++ Assets/Scripts/InventoryItem.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class InventoryItem
+public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    Transform parentAfterDrag;
     public ItemData data { get; private set; }
     public int stackSize { get; private set; }
 
@@ -21,5 +24,26 @@ public class InventoryItem
     public void DecreaseStack()
     {
         stackSize--;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = new Vector3(
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            transform.position.z
+        );
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(parentAfterDrag);
     }
 }
